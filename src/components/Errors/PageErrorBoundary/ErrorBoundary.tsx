5079e0af9a6f1b88b258d8ee2468a8f4505af64d
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2022-03-04 15:13:14 +0300

    feat(component): error boundary
    Error Boundary

--- src/components/Errors/PageErrorBoundary/ErrorBoundary.tsx
+++ src/components/Errors/PageErrorBoundary/ErrorBoundary.tsx
@@ -1,38 +1,91 @@
-import { Component, ReactChildren, ReactElement, ErrorInfo, ReactNode } from 'react';
+import {
+  isValidElement,
+  Component,
+  ReactChildren,
+  ReactElement,
+  ErrorInfo,
+  ReactNode,
+} from 'react';
 import { captureAndLogError } from '@monitoring';
+import { changedArray } from '@utils';
 import ErrorPage from '@pages/Error';
 import { ErrorBoundaryState, ErrorBoundaryProps } from './ErrorBoundary.interface';
 
 /**
- * A reusable component for handling errors in a React (sub)tree.
- * This will be the global error boundary for the application.
- * Errors that are not captured by sub level components will bubble up to here and be reported to a monitoring service.
+ * A reusable error boundary component for handling errors in a React application (sub)tree.
+ * This borrows ideas from the react-error-boundary package and aims to be extensible and re-useable across different components
+ * Errors will be reported to a monitoring service.
  */
 export default class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
   constructor(props: ErrorBoundaryProps) {
     super(props);
     this.state = {
       error: null,
-      hasError: false,
     };
   }
 
+  componentDidUpdate(prevProps: ErrorBoundaryProps, prevState: ErrorBoundaryState): void {
+    const { error } = this.state;
+    const { resetKeys, onResetKeysChange } = this.props;
+
+    // There's an edge case where if the thing that triggered the error
+    // happens to *also* be in the resetKeys array, we'd end up resetting
+    // the error boundary immediately. This would likely trigger a second
+    // error to be thrown.
+    // So we make sure that we don't check the resetKeys on the first call
+    // of cDU after the error is set
+
+    if (
+      error !== null &&
+      prevState.error !== null &&
+      changedArray(prevProps.resetKeys, resetKeys)
+    ) {
+      onResetKeysChange?.(prevProps.resetKeys, resetKeys);
+      this.reset();
+    }
+  }
+
   componentDidCatch(error: Error, errorInfo: ErrorInfo): void {
-    captureAndLogError(error, errorInfo);
+    const { onError } = this.props;
+    if (onError) {
+      onError(error, errorInfo);
+    } else {
+      captureAndLogError(error, errorInfo);
+    }
   }
 
   static getDerivedStateFromError(error: Error): ErrorBoundaryState {
     return {
-      hasError: true,
       error,
     };
   }
 
-  render(): ReactChildren | ReactElement | ReactNode {
-    const { children } = this.props;
-    const { error, hasError } = this.state;
+  resetErrorBoundary = (...args: unknown[]): void => {
+    const { onReset } = this.props;
+    onReset?.(...args);
+    this.reset();
+  };
 
-    if (error && hasError) {
+  reset(): void {
+    this.setState({ error: null });
+  }
+
+  render(): ReactChildren | ReactElement | ReactNode {
+    const { children, FallbackComponent, fallback } = this.props;
+    const { error } = this.state;
+
+    if (error) {
+      const props = {
+        error,
+        resetErrorBoundary: this.resetErrorBoundary,
+      };
+
+      if (isValidElement(fallback)) {
+        return fallback;
+      }
+      if (FallbackComponent) {
+        return <FallbackComponent {...props} />;
+      }
       return <ErrorPage />;
     }
     return children;
